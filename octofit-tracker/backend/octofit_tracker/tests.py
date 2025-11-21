f9a7726ea4ccbd369a818538e1d0c56b99b51d3c
Aravindm7377 <aravindm7377@gmail.com>
2025-11-21 15:36:46 +0000

    changes added

---
+++ octofit-tracker/backend/octofit_tracker/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from .models import User, Team, Activity, Workout, Leaderboard
+
+class ModelTests(TestCase):
+    def setUp(self):
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+        spiderman = User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel)
+        batman = User.objects.create(name='Batman', email='batman@dc.com', team=dc)
+        Activity.objects.create(user=spiderman, type='Running', duration=30, date='2025-11-21')
+        Workout.objects.create(name='Hero HIIT', description='High intensity interval training for heroes.')
+        Leaderboard.objects.create(team=marvel, points=100)
+
+    def test_user_count(self):
+        self.assertEqual(User.objects.count(), 2)
+    def test_team_count(self):
+        self.assertEqual(Team.objects.count(), 2)
+    def test_activity_count(self):
+        self.assertEqual(Activity.objects.count(), 1)
+    def test_workout_count(self):
+        self.assertEqual(Workout.objects.count(), 1)
+    def test_leaderboard_count(self):
+        self.assertEqual(Leaderboard.objects.count(), 1)
